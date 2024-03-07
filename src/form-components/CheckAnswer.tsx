ca1af4f3b2d3034c7d2746fe5aa723447b7a18d5
Jared <jem219@icloud.com>
2024-03-07 11:16:26 -0500

    check answers and give attempts finished

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,19 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [userAnswer, setUserAnswer] = useState("");
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setUserAnswer(event.target.value);
+    };
+
+    const isCorrect = userAnswer === expectedAnswer;
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <input type="text" onChange={handleChange}></input>
+            <div>{isCorrect ? "✔️" : "❌"}</div>
         </div>
     );
 }
