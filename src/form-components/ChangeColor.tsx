3c76fdb6e4287358d4adbeb900c19abeafedaf1b
Jared <jem219@icloud.com>
2024-03-07 12:47:34 -0500

    completed all files

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,45 @@
 import React, { useState } from "react";
 
 export function ChangeColor(): JSX.Element {
+    const colors = [
+        "Red",
+        "Blue",
+        "Green",
+        "Yellow",
+        "Purple",
+        "Pink",
+        "Orange",
+        "Black"
+    ];
+    const [selectedColor, setSelectedColor] = useState(colors[0]);
+
     return (
         <div>
             <h3>Change Color</h3>
+            {colors.map((color, index) => (
+                <div key={index}>
+                    <input
+                        type="radio"
+                        id={color}
+                        name="color"
+                        value={color}
+                        checked={selectedColor === color}
+                        onChange={() => setSelectedColor(color)}
+                    />
+                    <label htmlFor={color}>{color}</label>
+                </div>
+            ))}
+            <div
+                style={{
+                    backgroundColor: selectedColor,
+                    color: "white",
+                    padding: "10px",
+                    marginTop: "10px"
+                }}
+                data-testid="colored-box"
+            >
+                {selectedColor}
+            </div>
         </div>
     );
 }
